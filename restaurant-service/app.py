a21aa8cdc28e8471eb15f8f72df2b60a89d0e31a
Duvini27 <duvinidulwanya@gmail.com>
2026-03-27 00:16:40 +0530

    resturant service
    completed , db need to configure

---
+++ restaurant-service/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restx import Api
+from restaurant_routes import restaurant_ns
+from config import Config
+
+app = Flask(__name__)
+
+api = Api(
+    app,
+    title="Restaurant Service API",
+    version="1.0",
+    description="Handles restaurant operations",
+    doc="/docs"
+)
+
+api.add_namespace(restaurant_ns, path="/restaurants")
+
+if __name__ == "__main__":
+    app.run(port=Config.PORT, debug=True)
